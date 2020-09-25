6cb25bfce7c2390f22285227723fe7a72cd566be
Adrian Marin <adrian@adrianthedev.com>
2020-09-25 16:59:31 +0300

    refactor: change css import location

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '../css/application.css'
+import 'css/application.css'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
