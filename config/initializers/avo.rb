2f09138a1097f70988d40bbdd2c47654cde3c86e
Adrian Marin <adrian@adrianthedev.com>
2022-01-20 11:23:52 +0200

    set home_path

--- config/initializers/avo.rb
+++ config/initializers/avo.rb
@@ -3,6 +3,7 @@ Avo.configure do |config|
   config.license = 'pro'
   config.license_key = ENV['AVO_LICENSE_KEY']
   config.id_links_to_resource = true
+  config.home_path = '/avo/resources/posts'
   config.set_context do
     {
       foo: 'bar',
