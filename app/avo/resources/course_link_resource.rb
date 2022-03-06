f423ed73fd8e97fca8530c4cc5eda779911110c2
Adrian Marin <adrian@adrianthedev.com>
2022-03-06 18:59:44 +0200

    bump avo to 1.24.2

--- app/avo/resources/course_link_resource.rb
+++ app/avo/resources/course_link_resource.rb
@@ -8,12 +8,13 @@ class CourseLinkResource < Avo::BaseResource
   # end
 
   self.ordering = {
-    always_visible: true,
+    display_inline: true,
+    visible_on: :index,
     actions: {
-      higher: -> (record) { record.move_higher },
-      lower: -> (record) { record.move_lower },
-      to_top: -> (record) { record.move_to_top },
-      to_bottom: -> (record) { record.move_to_bottom },
+      higher: -> { record.move_higher },
+      lower: -> { record.move_lower },
+      to_top: -> { record.move_to_top },
+      to_bottom: -> { record.move_to_bottom },
     }
   }
 
