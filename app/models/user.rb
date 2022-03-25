21d52b95ca83e28a94bcfa11db2ead4836b81b28
Adrian Marin <adrian@adrianthedev.com>
2022-03-25 13:42:19 +0200

    add avatar and title

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,12 @@ class User < ApplicationRecord
   def notify(text)
     # notify about text
   end
+
+  def avatar
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  end
+
+  def avo_title
+    'Admin'
+  end
 end
