9f1ad60254c1dffdb3291f53297900eaed53b314
Adrian Marin <adrian@adrianthedev.com>
2022-04-28 13:45:08 +0300

    add famous users

--- app/services/seed_service.rb
+++ app/services/seed_service.rb
@@ -46,6 +46,73 @@ class SeedService
       users.push(FactoryBot.create(:user, team_id: teams.sample.id))
     end
 
+    famous_users = [
+      {
+        first_name: 'David Heinemeier',
+        last_name: 'Hansson',
+        email: 'david@hey.com'
+      },
+      {
+        first_name: 'Chris',
+        last_name: 'Oliver',
+        email: 'chris@gorails.com'
+      },
+      {
+        first_name: 'Jason',
+        last_name: 'Charnes',
+        email: 'jason@jasoncharnes.com'
+      },
+      {
+        first_name: 'Jason',
+        last_name: 'Swett',
+        email: 'jason@benfranklinlabs.com'
+      },
+      {
+        first_name: 'Yukihiro "Matz"',
+        last_name: 'Matsumoto',
+        email: 'matz@ruby.or.jp'
+      },
+      {
+        first_name: 'Joe',
+        last_name: 'Masilotti',
+        email: 'joe@masilotti.com'
+      },
+      {
+        first_name: 'Lucian',
+        last_name: 'Ghinda',
+        email: 'lucian@ghinda.com'
+      },
+      {
+        first_name: 'Mike',
+        last_name: 'Perham',
+        email: 'mperham@gmail.com'
+      },
+      {
+        first_name: 'Taylor',
+        last_name: 'Otwell',
+        email: 'taylor@laravel.com'
+      },
+      {
+        first_name: 'Adam',
+        last_name: 'Watham',
+        email: 'adam@adamwathan.me'
+      },
+      {
+        first_name: 'Jeffery',
+        last_name: 'Way',
+        email: 'jeffrey@laracasts.com'
+      },
+      {
+        first_name: 'Adrian',
+        last_name: 'Marin',
+        email: 'adrian@adrianthedev.com'
+      },
+    ]
+
+    famous_users.reverse.each do |user|
+      users.push(FactoryBot.create(:user, team_id: teams.sample.id, **user))
+    end
+
     users.push User.create(
       first_name: "Avo",
       last_name: "Cado",
