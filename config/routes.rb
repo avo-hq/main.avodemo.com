4d7dd9e9b4642077c988227ef5912b0a4c4a5667
Adrian <adrian@adrianthedev.com>
2023-04-13 11:31:51 +0300

    update Avo

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: redirect('/avo')
+  root to: redirect("/avo")
 
   get "hey", to: "home#hey"
 
-  post '/reset', to: 'home#reset'
+  post "/reset", to: "home#reset"
 
-  authenticate :user, -> user { user.admin? } do
+  authenticate :user, ->(user) { user.admin? } do
     mount Avo::Engine => Avo.configuration.root_path
   end
+  # scope ":course", constraints: {course: /\w+(-\w+)*/} do
+  #   scope ":locale", constraints: {locale: /\w[-\w]*/} do
+  #     get "hey", to: "home#hey"
+  #     mount Avo::Engine, at: Avo.configuration.root_path
+  #   end
+  # end
 
-  get :checkcheckcheck, to: 'home#check'
+  get :checkcheckcheck, to: "home#check"
 end
 
 if defined? ::Avo
   Avo::Engine.routes.draw do
-    get 'custom_page', to: "tools#custom_page"
+    get "custom_page", to: "tools#custom_page"
 
     scope :resources do
       get "courses/cities", to: "courses#cities"
