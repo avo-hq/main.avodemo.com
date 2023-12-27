d18f32538db6afa7fa8997e241bd00f3f0b2412b
Adrian <adrian@adrianthedev.com>
2023-12-27 20:27:54 +0200

    make everything ransackable

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # `ransackable_attributes` by default returns all column names
+  # and any defined ransackers as an array of strings.
+  # For overriding with a whitelist array of strings.
+  #
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  # `ransackable_associations` by default returns the names
+  # of all associations as an array of strings.
+  # For overriding with a whitelist array of strings.
+  #
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
 end
