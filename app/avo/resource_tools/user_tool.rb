c8ecb9b53a770103a993df4c2b3acec0a1faf737
Adrian <adrian@adrianthedev.com>
2024-01-06 19:18:35 +0200

    show how you can access tool variables and methods

--- app/avo/resource_tools/user_tool.rb
+++ app/avo/resource_tools/user_tool.rb
@@ -1,4 +1,16 @@
 class Avo::ResourceTools::UserTool < Avo::BaseResourceTool
   self.name = "User tool"
   # self.partial = "avo/resource_tools/user_tool"
+
+  attr_reader :foo
+
+  def initialize(**kwargs)
+    super **kwargs
+
+    @foo = :bar
+  end
+
+  def custom_method_call
+    :called
+  end
 end
