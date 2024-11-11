7eb1fa69b37edcc8f7403970e706a9842dfdf224
Paul Bob <paul.ionut.bob@gmail.com>
2024-11-11 14:09:19 +0200

    add app_revision env var for appsignal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+ENV["APP_REVISION"] = ENV["HATCHBOX_REVISION"]
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
