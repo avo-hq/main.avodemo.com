fd668c6e038b76fca7dac00594674296aee38179
Paul Bob <paul.ionut.bob@gmail.com>
2024-12-10 11:50:37 +0200

    Revert "try redis session store"
    This reverts commit 854e33a1a3b715e41dff5b7454344662c0b73c35.

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
 gem "redis", "~> 4.0"
-gem 'redis-rails'
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  config.session_store :redis_store, servers: "#{ENV.fetch("REDIS_URL")}/session"
-
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "avodemo_7_production"
