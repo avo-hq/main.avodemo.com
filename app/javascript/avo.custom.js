e772a4abcda0ed99b6365193bfbe66bbf551e088
Adrian Marin <adrian@adrianthedev.com>
2025-01-23 16:46:16 +0200

    add list continuation

--- app/javascript/avo.custom.js
+++ app/javascript/avo.custom.js
@@ -1,12 +1,13 @@
 import CourseResourceController from 'controllers/course_controller'
 import NestedForm from 'stimulus-rails-nested-form'
-import { MarksmithController } from '@avo-hq/marksmith'
+import { MarksmithController, ListContinuationController } from '@avo-hq/marksmith'
 
 // Hook into the stimulus instance provided by Avo
 const application = window.Stimulus
 application.register('course-resource', CourseResourceController)
 application.register('nested-form', NestedForm)
 application.register('marksmith', MarksmithController)
+application.register('list-continuation', ListContinuationController)
 
 // eslint-disable-next-line no-console
 console.log('Hi from Avo custom JS 👋')
