7a05e8833ee763bf554702b1ab44f89c84f06891
Paul Bob <paul.ionut.bob@gmail.com>
2025-01-30 22:20:08 +0200

    update to 3.16.4

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,9 +3,3 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-
-// We can remove this line after upgrading to Avo 3.12
-//= link avo-rhino_field
-
-// Temporary
-//= link late-registration.js
