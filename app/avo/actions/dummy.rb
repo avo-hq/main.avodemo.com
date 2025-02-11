5e4160d58dd207293b258b130395c49c41d28a0e
Paul Bob <paul.ionut.bob@gmail.com>
2025-02-11 13:00:46 +0200

    update avo

--- app/avo/actions/dummy.rb
+++ app/avo/actions/dummy.rb
@@ -3,6 +3,10 @@ class Avo::Actions::Dummy < Avo::BaseAction
   self.standalone = true
   self.visible = -> { view.index? }
 
+  def fields
+    field :size, as: :radio, options: {small: "Small Option", medium: "Medium Option", large: "Large Option"}
+  end
+
   def handle(**args)
     # Do something here
 
