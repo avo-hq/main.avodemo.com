1fb4174cae10f0cdda319b181ed6748466c29d86
Paul Bob <paul.ionut.bob@gmail.com>
2025-10-07 15:46:21 +0300

    improve filter

--- app/avo/filters/active.rb
+++ app/avo/filters/active.rb
@@ -2,6 +2,8 @@ class Avo::Filters::Active < Avo::Filters::BooleanFilter
   self.name = "Is Active"
 
   def apply(request, query, value)
+    return query if value['is_active'] && value['is_inactive']
+
     query.where(active: value[:is_active])
   end
 
